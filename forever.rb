278865f0e39dedb9cc62ccb6c32988c61b5ee6f1
pandoralong <mytrace@126.com>
2016-03-09 19:29:04 +0800

    my second file to commit

---
+++ forever.rb
@@ -0,0 +1,4 @@
+# 1. I should be the man who i want to be.
+# 2. If you want to be the man who you want to be ,
+#    you should work hard, never give up even you meet some troubles.
+# 3. I believe myself, i can make it.
