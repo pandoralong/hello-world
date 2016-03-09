6da9b4260b1e100674e754bf28438a994d90eb35
pandoralong <mytrace@126.com>
2016-03-09 20:21:52 +0800

    Want to push to remote

--- forever.rb
+++ forever.rb
@@ -2,3 +2,4 @@
 # 2. If you want to be the man who you want to be ,
 #    you should work hard, never give up even you meet some troubles.
 # 3. I believe myself, i can make it.
+# 4. Push local to remote.
